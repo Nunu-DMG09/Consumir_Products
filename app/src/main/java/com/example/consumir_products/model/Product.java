961037c4e75c7abe7882c3750756eceb80ecfb57
Nunu-DMG09 <davidmesta09@gmail.com>
2025-09-06 17:36:22 -0500

    add models

---
+++ app/src/main/java/com/example/consumir_products/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.consumir_products.model;
+
+import java.util.List;
+
+public class Product {
+    public int id;
+    public String title;
+    public String description;
+    public double price;
+    public List<String> images;
+}
---
+++ app/src/main/java/com/example/consumir_products/model/ProductsResponse.java
@@ -0,0 +1,7 @@
+package com.example.consumir_products.model;
+
+import java.util.List;
+
+public class ProductsResponse {
+    public List<Product> products;
+}
